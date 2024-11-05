59774b0f951b35478e9166445ddcad166fb66aa2
onnanoco <moe@onnano.co>
2024-11-05 10:31:01 +0900

    initial commit

--- contracts/MOESoulboundToken.sol
+++ contracts/MOESoulboundToken.sol
@@ -3,6 +3,25 @@ pragma solidity 0.8.28;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+struct Artist {
+    string name;
+    string uri;
+}
+
 contract MOESoulboundToken {
+
+    mapping (address => Artist) public artists;
+
+    function _update(address to, uint256 tokenId, address auth) internal virtual override {
+        
+        address from = _ownerOf(tokenId);
+
+        if (from != address(0) && to != address(0)){
+            revert("MOESBT: Transfer failed");
+        }
+
+        return super._update(to, tokenId, auth);
+    }
+
 
 }
